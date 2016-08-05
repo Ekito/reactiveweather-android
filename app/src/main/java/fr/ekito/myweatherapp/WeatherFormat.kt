ebdfef7b38b2713ffba2efdd381a4c785f8e977c
Arnaud Giuliani <agiuliani@ekito.fr>
2016-08-05 11:13:04 +0200

    fun kotlin

--- app/src/main/java/fr/ekito/myweatherapp/WeatherFormat.kt
+++ app/src/main/java/fr/ekito/myweatherapp/WeatherFormat.kt
@@ -1,34 +1,20 @@
 package fr.ekito.myweatherapp
 
 import android.widget.TextView
-
-import java.util.ArrayList
-
 import fr.ekito.myweatherlibrary.json.weather.Forecastday_
 
 /**
  * Created by arnaud on 04/08/2016.
  */
 object WeatherFormat {
-
-
+    
     fun weatherTxt(forecastday: Forecastday_): String {
         return forecastday.conditions //.getTitle() + "\n" + forecastday.getFcttextMetric();
     }
 
     fun filterForecast(forecastday: List<Forecastday_>): List<Forecastday_> {
-        val filtered = ArrayList<Forecastday_>()
-
-        println("")
-        for (f in forecastday) {
-            if (!f.icon.startsWith("nt_")) {
-                filtered.add(f)
-                if (filtered.size >= 4) {
-                    break
-                }
-            }
-        }
-        return filtered
+        return forecastday.filter { f -> !f.icon.startsWith("nt_") }
+                .take(4)
     }
 
     fun getTemp(day1: Forecastday_): String {
@@ -42,13 +28,11 @@ object WeatherFormat {
     }
 
     fun getWeatherCode(icon: String): String {
-        var icon = icon
-        icon = icon.replace("nt_".toRegex(), "")
-        if (icon == "cloudy") {
-            return Character.toString(0x049.toChar())
-        } else if (icon == "partlycloudy") {
-            return Character.toString(0x022.toChar())
-        } else
-            return Character.toString(0x049.toChar())
+        val icon = icon.replace("nt_".toRegex(), "")
+        when (icon) {
+            "cloudy" -> return Character.toString(0x049.toChar())
+            "partlycloudy" -> return Character.toString(0x022.toChar())
+            else -> return Character.toString(0x049.toChar())
+        }
     }
 }
