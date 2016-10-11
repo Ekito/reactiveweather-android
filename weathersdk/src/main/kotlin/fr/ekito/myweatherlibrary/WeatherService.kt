ab4ac86738363d5154e2636bbd6275667fea0266
Arnaud Giuliani <agiuliani@ekito.fr>
2016-10-11 16:30:49 +0200

    rewriting json in kotlin

---
+++ weathersdk/src/main/kotlin/fr/ekito/myweatherlibrary/WeatherService.kt
@@ -0,0 +1,42 @@
+package fr.ekito.myweatherlibrary
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import fr.ekito.myweatherlibrary.di.KInjector
+import fr.ekito.myweatherlibrary.json.geocode.Geocode
+import fr.ekito.myweatherlibrary.json.weather.Weather
+import fr.ekito.myweatherlibrary.ws.WeatherWS
+import rx.Observable
+
+class WeatherService : Service() {
+
+    val TAG = WeatherService::class.java.simpleName
+    val mBinder = LocalBinder()
+    lateinit var weatherWS: WeatherWS
+
+    override fun onBind(intent: Intent): IBinder? {
+        // inject stuff
+        weatherWS = KInjector.weatherWS
+        return mBinder
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        // close stuff
+        return super.onUnbind(intent)
+    }
+
+    inner class LocalBinder : Binder() {
+        val service: WeatherService
+            get() = this@WeatherService
+    }
+
+    fun geocode(address: String): Observable<Geocode> {
+        return weatherWS.geocode(address)
+    }
+
+    fun weather(lat: Double, lon: Double, lang: String): Observable<Weather> {
+        return weatherWS.weather(lat, lon, lang)
+    }
+}
---
+++ weathersdk/src/main/kotlin/fr/ekito/myweatherlibrary/json/geocode/Result.kt
@@ -0,0 +1,59 @@
+package fr.ekito.myweatherlibrary.json.geocode
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+import java.util.ArrayList
+
+import fr.ekito.myweatherlibrary.json.geocode.AddressComponent
+import fr.ekito.myweatherlibrary.json.geocode.Geometry
+
+class Result {
+
+    /**
+     * @return The addressComponents
+     */
+    /**
+     * @param addressComponents The address_components
+     */
+    @SerializedName("address_components")
+    @Expose
+    var addressComponents: List<AddressComponent> = ArrayList()
+    /**
+     * @return The formattedAddress
+     */
+    /**
+     * @param formattedAddress The formatted_address
+     */
+    @SerializedName("formatted_address")
+    @Expose
+    var formattedAddress: String? = null
+    /**
+     * @return The geometry
+     */
+    /**
+     * @param geometry The geometry
+     */
+    @SerializedName("geometry")
+    @Expose
+    var geometry: Geometry? = null
+    /**
+     * @return The placeId
+     */
+    /**
+     * @param placeId The place_id
+     */
+    @SerializedName("place_id")
+    @Expose
+    var placeId: String? = null
+    /**
+     * @return The types
+     */
+    /**
+     * @param types The types
+     */
+    @SerializedName("types")
+    @Expose
+    var types: List<String> = ArrayList()
+
+}
